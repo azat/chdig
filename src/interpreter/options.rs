2f78d17b5dc14279f93f534fffcac60f8b0ff4e8
Azat Khuzhin <a3at.mail@gmail.com>
2024-03-31 14:32:13 +0200

    Fix error message for invalid DateTime parsing of --begin/--end options

--- src/interpreter/options.rs
+++ src/interpreter/options.rs
@@ -115,7 +115,7 @@ pub fn parse_datetime(value: &str) -> Result<DateTime<Local>, String> {
     } else {
         let date = value
             .parse::<NaiveDate>()
-            .map_err(|err| format!("valid RFC3339-formatted (YYYY-MM-DDTHH:MM:SS[.ssssss][±hh:mm]) date or datetime: {err}"))?;
+            .map_err(|err| format!("valid RFC3339-formatted (YYYY-MM-DDTHH:MM:SS[.ssssss][±hh:mm]|Z) date or datetime: {err}"))?;
         Ok(date
             .and_hms_opt(0, 0, 0)
             .unwrap()
