7c997f904c5ad4404f52081ace37bfa0b29484e8
Azat Khuzhin <a3at.mail@gmail.com>
2024-04-01 15:59:15 +0200

    Fix panic in case of resizing the window with active logs search

--- src/view/log_view.rs
+++ src/view/log_view.rs
@@ -151,6 +151,13 @@ impl LogViewBase {
     }
 
     fn update_search(&mut self) -> Option<EventResult> {
+        // In case of resize we can have less rows then before,
+        // so reset the matched_row for this scenario to avoid out-of-bound access.
+        if let Some(rows) = self.rows.as_ref() {
+            if rows.len() < self.matched_row.unwrap_or_default() {
+                self.matched_row = None;
+            }
+        }
         if self.search_direction_forward {
             return self.update_search_forward();
         } else {
