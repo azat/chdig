azat/chdig

commit 5e0d54b0e7000a19433dbe8aed83e6e4b2d3decd

Add Inserted/Selected rows into summary

src/interpreter/clickhouse.rs
@@ -101,6 +101,11 @@ pub struct ClickHouseServerStorages {
     pub distributed_insert_files: u64,
 }
 #[derive(Default)]
+pub struct ClickHouseServerRows {
+    pub selected: u64,
+    pub inserted: u64,
+}
+#[derive(Default)]
 pub struct ClickHouseServerSummary {
     pub processes: u64,
     pub merges: u64,
@@ -109,6 +114,7 @@ pub struct ClickHouseServerSummary {
     pub replication_queue_tries: u64,
     pub fetches: u64,
     pub servers: u64,
+    pub rows: ClickHouseServerRows,
     pub storages: ClickHouseServerStorages,
     pub uptime: ClickHouseServerUptime,
     pub memory: ClickHouseServerMemory,
@@ -396,6 +402,7 @@ impl ClickHouse {
                         assumeNotNull(memory_dictionaries_)                      AS memory_dictionaries,
 
                         asynchronous_metrics.*,
+                        events.*,
                         metrics.*
                     FROM
                     (
@@ -433,6 +440,12 @@ impl ClickHouse {
                             CAST(anyLastIf(value, metric == 'AsynchronousMetricsUpdateInterval') AS UInt64) AS metrics_update_interval
                         FROM {asynchronous_metrics}
                     ) as asynchronous_metrics,
+                    (
+                        SELECT
+                            sumIf(CAST(value AS UInt64), event == 'SelectedRows') AS selected_rows,
+                            sumIf(CAST(value AS UInt64), event == 'InsertedRows') AS inserted_rows
+                        FROM {events}
+                    ) as events,
                     (
                         SELECT
                             sumIf(CAST(value AS UInt64), metric == 'StorageBufferBytes') AS storage_buffer_bytes,
@@ -474,6 +487,7 @@ impl ClickHouse {
                     SETTINGS enable_global_with_statement=0
                 "#,
                     metrics=self.get_table_name("system.metrics"),
+                    events=self.get_table_name("system.events"),
                     tables=self.get_table_name("system.tables"),
                     processes=self.get_table_name("system.processes"),
                     merges=self.get_table_name("system.merges"),
@@ -501,6 +515,11 @@ impl ClickHouse {
             uptime: ClickHouseServerUptime {
                 _os: get("os_uptime"),
                 server: get("uptime"),
+            },
+
+            rows: ClickHouseServerRows {
+                selected: get("selected_rows"),
+                inserted: get("inserted_rows"),
             },
 
             storages: ClickHouseServerStorages {

src/view/summary_view.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Local};
 use cursive::{
     event::{AnyCb, Event, EventResult},
     theme::BaseColor,
@@ -15,6 +16,9 @@ use crate::interpreter::{
 };
 
 pub struct SummaryView {
+    prev_summary: Option<ClickHouseServerSummary>,
+    prev_update_time: Option<DateTime<Local>>,
+
     layout: views::LinearLayout,
 
     #[allow(unused)]
@@ -149,7 +153,21 @@ impl SummaryView {
                         BaseColor::Cyan.dark(),
                     )))
                     .child(views::DummyView.fixed_width(1))
-                    .child(views::TextView::new("").with_name("disk_write")),
+                    .child(views::TextView::new("").with_name("disk_write"))
+                    .child(views::DummyView.fixed_width(1))
+                    .child(views::TextView::new(StyledString::styled(
+                        "Selected rows:",
+                        BaseColor::Cyan.dark(),
+                    )))
+                    .child(views::DummyView.fixed_width(1))
+                    .child(views::TextView::new("").with_name("selected_rows"))
+                    .child(views::DummyView.fixed_width(1))
+                    .child(views::TextView::new(StyledString::styled(
+                        "Inserted rows:",
+                        BaseColor::Cyan.dark(),
+                    )))
+                    .child(views::DummyView.fixed_width(1))
+                    .child(views::TextView::new("").with_name("inserted_rows")),
             )
             .child(
                 views::LinearLayout::horizontal()
@@ -181,7 +199,12 @@ impl SummaryView {
         let mut bg_runner = BackgroundRunner::new(delay, bg_runner_cv);
         bg_runner.start(update_callback);
 
-        return Self { layout, bg_runner };
+        return Self {
+            prev_summary: None,
+            prev_update_time: None,
+            layout,
+            bg_runner,
+        };
     }
 
     pub fn set_view_content<S>(&mut self, view_name: &str, content: S)
@@ -207,6 +230,13 @@ impl SummaryView {
         } else {
             1
         };
+        let now = Local::now();
+        let mut since_prev_us = (now - self.prev_update_time.unwrap_or(Local::now()))
+            .num_microseconds()
+            .unwrap_or_default() as u64;
+        if since_prev_us == 0 {
+            since_prev_us = 1;
+        }
 
         {
             let mut description: Vec<String> = Vec::new();
@@ -311,6 +341,17 @@ impl SummaryView {
             fmt_ref.format((summary.blkdev.write_bytes / update_interval) as i64),
         );
 
+        let mut selected_rows = summary.rows.selected / summary.uptime.server;
+        let mut inserted_rows = summary.rows.inserted / summary.uptime.server;
+        if let Some(prev_summary) = &self.prev_summary {
+            selected_rows =
+                (summary.rows.selected - prev_summary.rows.selected) * 1_000_000 / since_prev_us;
+            inserted_rows =
+                (summary.rows.inserted - prev_summary.rows.inserted) * 1_000_000 / since_prev_us;
+        }
+        self.set_view_content("selected_rows", fmt_ref.format(selected_rows as i64));
+        self.set_view_content("inserted_rows", fmt_ref.format(inserted_rows as i64));
+
         self.set_view_content(
             "uptime",
             format_duration(Duration::from_secs(summary.uptime.server)).to_string(),
@@ -389,6 +430,9 @@ impl SummaryView {
             );
             self.set_view_content("storage_distributed_insert_files", content);
         }
+
+        self.prev_summary = Some(summary);
+        self.prev_update_time = Some(now);
     }
 }
 

commit dea313517d1f740ebf9576edb4daee62668bd980

Fix compatibility with ClickHouse without allow_experimental_analyzer=1
ClickHouse without allow_experimental_analyzer returns name of the
subquery with the key when you have > 2 subqueries (odd):

This is for 21.2:

    select a.*, b.*, c.* from (select * from system.one) as a, (select dummy as x from system.one) as b, (select dummy as y from system.one) as c
    ┌─a.dummy─┬─b.x─┬─c.y─┐
    │       0 │   0 │   0 │
    └─────────┴─────┴─────┘

    select a.*, b.* from (select * from system.one) as a, (select dummy as x from system.one) as b
    ┌─dummy─┬─x─┐
    │     0 │ 0 │
    └───────┴───┘

24.7 upstream:

    select a.*, b.*, c.* from (select * from system.one) as a, (select dummy as x from system.one) as b, (select dummy as y from system.one) as c
    ┌─a.dummy─┬─b.x─┬─c.y─┐
    │       0 │   0 │   0 │
    └─────────┴─────┴─────┘

    select a.*, b.*, c.* from (select * from system.one) as a, (select dummy as x from system.one) as b, (select dummy as y from system.one) as c settings allow_experimental_analyzer=0
    ┌─a.dummy─┬─b.x─┬─c.y─┐
    │       0 │   0 │   0 │
    └─────────┴─────┴─────┘

Fixes: 5e0d54b ("Add Inserted/Selected rows into summary")

src/interpreter/clickhouse.rs
@@ -501,7 +501,17 @@ impl ClickHouse {
             )
             .await?;
 
-        let get = |key: &str| block.get::<u64, _>(0, key).expect(key);
+        let get = |key: &str| {
+            // By subquery.column
+            if let Ok(value) = block.get::<u64, _>(0, key) {
+                return value;
+            }
+
+            let parts = key.split(".").collect::<Vec<&str>>();
+            assert!(parts.len() <= 2);
+            // By column
+            return block.get::<u64, _>(0, parts[parts.len() - 1]).expect(key);
+        };
 
         return Ok(ClickHouseServerSummary {
             processes: get("processes"),
@@ -513,23 +523,23 @@ impl ClickHouse {
             servers: get("servers"),
 
             uptime: ClickHouseServerUptime {
-                _os: get("os_uptime"),
-                server: get("uptime"),
+                _os: get("asynchronous_metrics.os_uptime"),
+                server: get("asynchronous_metrics.uptime"),
             },
 
             rows: ClickHouseServerRows {
-                selected: get("selected_rows"),
-                inserted: get("inserted_rows"),
+                selected: get("events.selected_rows"),
+                inserted: get("events.inserted_rows"),
             },
 
             storages: ClickHouseServerStorages {
-                buffer_bytes: get("storage_buffer_bytes"),
-                distributed_insert_files: get("storage_distributed_insert_files"),
+                buffer_bytes: get("metrics.storage_buffer_bytes"),
+                distributed_insert_files: get("metrics.storage_distributed_insert_files"),
             },
 
             memory: ClickHouseServerMemory {
-                os_total: get("os_memory_total"),
-                resident: get("memory_resident"),
+                os_total: get("asynchronous_metrics.os_memory_total"),
+                resident: get("asynchronous_metrics.memory_resident"),
 
                 tracked: get("memory_tracked"),
                 tables: get("memory_tables"),
@@ -537,48 +547,48 @@ impl ClickHouse {
                 processes: get("memory_processes"),
                 merges: get("memory_merges"),
                 dictionaries: get("memory_dictionaries"),
-                primary_keys: get("memory_primary_keys"),
+                primary_keys: get("asynchronous_metrics.memory_primary_keys"),
             },
 
             cpu: ClickHouseServerCPU {
-                count: get("cpu_count"),
-                user: get("cpu_user"),
-                system: get("cpu_system"),
+                count: get("asynchronous_metrics.cpu_count"),
+                user: get("asynchronous_metrics.cpu_user"),
+                system: get("asynchronous_metrics.cpu_system"),
             },
 
             threads: ClickHouseServerThreads {
-                os_total: get("threads_os_total"),
-                os_runnable: get("threads_os_runnable"),
-                http: get("threads_http"),
-                tcp: get("threads_tcp"),
-                interserver: get("threads_interserver"),
+                os_total: get("asynchronous_metrics.threads_os_total"),
+                os_runnable: get("asynchronous_metrics.threads_os_runnable"),
+                http: get("asynchronous_metrics.threads_http"),
+                tcp: get("asynchronous_metrics.threads_tcp"),
+                interserver: get("asynchronous_metrics.threads_interserver"),
                 pools: ClickHouseServerThreadPools {
-                    merges_mutations: get("threads_merges_mutations"),
-                    fetches: get("threads_fetches"),
-                    common: get("threads_common"),
-                    moves: get("threads_moves"),
-                    schedule: get("threads_schedule"),
-                    buffer_flush: get("threads_buffer_flush"),
-                    distributed: get("threads_distributed"),
-                    message_broker: get("threads_message_broker"),
-                    backups: get("threads_backups"),
-                    io: get("threads_io"),
-                    remote_io: get("threads_remote_io"),
-                    queries: get("threads_queries"),
+                    merges_mutations: get("metrics.threads_merges_mutations"),
+                    fetches: get("metrics.threads_fetches"),
+                    common: get("metrics.threads_common"),
+                    moves: get("metrics.threads_moves"),
+                    schedule: get("metrics.threads_schedule"),
+                    buffer_flush: get("metrics.threads_buffer_flush"),
+                    distributed: get("metrics.threads_distributed"),
+                    message_broker: get("metrics.threads_message_broker"),
+                    backups: get("metrics.threads_backups"),
+                    io: get("metrics.threads_io"),
+                    remote_io: get("metrics.threads_remote_io"),
+                    queries: get("metrics.threads_queries"),
                 },
             },
 
             network: ClickHouseServerNetwork {
-                send_bytes: get("net_send_bytes"),
-                receive_bytes: get("net_receive_bytes"),
+                send_bytes: get("asynchronous_metrics.net_send_bytes"),
+                receive_bytes: get("asynchronous_metrics.net_receive_bytes"),
             },
 
             blkdev: ClickHouseServerBlockDevices {
-                read_bytes: get("block_read_bytes"),
-                write_bytes: get("block_write_bytes"),
+                read_bytes: get("asynchronous_metrics.block_read_bytes"),
+                write_bytes: get("asynchronous_metrics.block_write_bytes"),
             },
 
-            update_interval: get("metrics_update_interval"),
+            update_interval: get("asynchronous_metrics.metrics_update_interval"),
         });
     }
 