0c1f9418b37e2767935d1ef2f5553b6c6aaf5e00
Azat Khuzhin <a3at.mail@gmail.com>
2025-11-10 21:33:10 +0100

    Do not exit from chdig by Esc (too error prone)

--- src/view/navigation.rs
+++ src/view/navigation.rs
@@ -413,7 +413,7 @@ impl Navigation for Cursive {
                 toggle_flexi_logger_debug_console,
             );
         }
-        context.add_global_action(self, "Back/Quit", Key::Esc, |siv| siv.pop_ui(true));
+        context.add_global_action(self, "Back/Quit", Key::Esc, |siv| siv.pop_ui(false));
         context.add_global_action(self, "Back/Quit", 'q', |siv| siv.pop_ui(true));
         context.add_global_action(self, "Quit forcefully", 'Q', |siv| siv.quit());
         context.add_global_action(self, "Back", Key::Backspace, |siv| siv.pop_ui(false));
