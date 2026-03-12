70bf834a89477b1a32589f2a05d73876d0420164
Azat Khuzhin <a3at.mail@gmail.com>
2026-03-12 09:32:13 +0100

    Revert "Print stderr of client in case of failures"
    It is not possible to do correctly, since client needs pty/tty, and we
    do not want to implement full tmux support here
    
    This reverts commit c6e75147fee3cdae85557a87db60f2807ecf2a00.

--- src/view/providers/client.rs
+++ src/view/providers/client.rs
@@ -141,8 +141,6 @@ impl ViewProvider for ClientViewProvider {
         let cmd_line = format!("{:?}", cmd);
         log::info!("Spawning client: {}", cmd_line);
 
-        cmd.stderr(std::process::Stdio::piped());
-
         let result = {
             let _guard = TerminalRawModeGuard::leave();
             eprintln!("\n--- chdig: launching clickhouse client ---\n");
@@ -150,26 +148,21 @@ impl ViewProvider for ClientViewProvider {
             // Ignore SIGINT in chdig while the child runs, so Ctrl-C only reaches
             // the clickhouse client (same semantics as a shell foreground job).
             let prev_handler = unsafe { libc::signal(libc::SIGINT, libc::SIG_IGN) };
-            let output = cmd.spawn().and_then(|child| child.wait_with_output());
+            let status = cmd.spawn().and_then(|mut child| child.wait());
             unsafe { libc::signal(libc::SIGINT, prev_handler) };
-            output
+            status
         };
 
         match result {
-            Ok(output) => {
-                let stderr = String::from_utf8_lossy(&output.stderr).to_string();
+            Ok(status) => {
                 cb_sink
                     .send(Box::new(move |siv| {
                         siv.complete_clear();
-                        if !output.status.success() {
-                            let mut msg = format!(
+                        if !status.success() {
+                            siv.add_layer(Dialog::info(format!(
                                 "clickhouse client exited with status: {}\n\nCommand: {}",
-                                output.status, cmd_line
-                            );
-                            if !stderr.is_empty() {
-                                msg.push_str(&format!("\n\nStderr:\n{}", stderr));
-                            }
-                            siv.add_layer(Dialog::info(msg));
+                                status, cmd_line
+                            )));
                         }
                     }))
                     .ok();
